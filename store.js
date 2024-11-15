47c273c1dfc075f7242b35bc9e62172718357d85
Aadam Mazema <maze.aadam@gmail.com>
2024-11-15 18:31:13 +0200

    mplement createStore function
    setting up methods to get state, dispatch actions and subscribe to state changes through the reducer. Subscribes listeners to react to state changes.

--- store.js
+++ store.js
@@ -8,3 +8,38 @@ const initialState = {
   const SUBTRACT = 'SUBTRACT';
   const RESET = 'RESET';
   
+  // Create Store Function
+  function createStore(reducer, initialState) {
+    let state = initialState;
+    const listeners = [];
+  
+    // Get the current state
+    function getState() {
+      return state;
+    }
+  
+    // Dispatch an action to update the state
+    function dispatch(action) {
+      state = reducer(state, action);
+      listeners.forEach(listener => listener());
+    }
+  
+    // Subscribe to state changes
+    function subscribe(listener) {
+      listeners.push(listener);
+      // Return a function to unsubscribe
+      return () => {
+        const index = listeners.indexOf(listener);
+        if (index >= 0) {
+          listeners.splice(index, 1);
+        }
+      };
+    }
+  
+    return {
+      getState,
+      dispatch,
+      subscribe,
+    };
+  }
+  
